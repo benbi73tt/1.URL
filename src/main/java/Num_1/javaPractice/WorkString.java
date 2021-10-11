c1e53dc01daafcf1fc5c9ee8a846f47ec50a2073
Anton <56966986+benbi73tt@users.noreply.github.com>
2021-10-11 14:06:51 +0400

    sort

--- src/main/java/Num_1/javaPractice/WorkString.java
+++ src/main/java/Num_1/javaPractice/WorkString.java
@@ -1,5 +1,6 @@
 package Num_1.javaPractice;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WorkString implements InterfaceParser {
@@ -12,18 +13,29 @@ public class WorkString implements InterfaceParser {
         return arr.length;
     }
 
-    public Map<String, Integer> duplicate() {
+//todo a choice:
+//    public Map<String, Integer> duplicate() {
+//        Map<String,Integer > counter = new HashMap<>();
+//        for(String x: string){
+//            int newValue = counter.getOrDefault(x,0) + 1;
+//            counter.put(x,newValue);
+//        }
+//        return counter;
+//    }
+//    public Stream<Map.Entry<String, Integer>> sorting(){
+//        Map<String, Integer> sort = duplicate();
+//        return sort.entrySet().stream().sorted(Map.Entry.<String,Integer>comparingByValue()
+//                .reversed());
+//    }
+
+
+    public Stream<Map.Entry<String, Integer>> duplicate() {
         Map<String,Integer > counter = new HashMap<>();
         for(String x: string){
             int newValue = counter.getOrDefault(x,0) + 1;
             counter.put(x,newValue);
         }
-        return counter;
-    }
-
-    public Stream<Map.Entry<String, Integer>> sorting(){
-        Map<String, Integer> sort = duplicate();
-        return sort.entrySet().stream().sorted(Map.Entry.<String,Integer>comparingByValue()
+        return counter.entrySet().stream().sorted(Map.Entry.<String,Integer>comparingByValue()
                 .reversed());
     }
 
@@ -36,7 +48,8 @@ public class WorkString implements InterfaceParser {
         Collections.addAll(string, arr);
         System.out.println("Все имеющиеся слова в тексте: " + allWord());
         System.out.println("Сколько и какие слова повторяются: " );
-        sorting().forEach(System.out::println);
+        duplicate().forEach((System.out::println));
+        //todo sorting().forEach(System.out::println);
         System.out.println("Количество слов в файле: " + countWord());
     }
 }
